c6eb43ccb04eeeb288019715c268c891007f99a1
sadmagrage <victorgoyapvgp09@gmail.com>
2023-04-01 17:55:21 -0300

    added circle progress

--- js/js.js
+++ js/js.js
@@ -12,5 +12,19 @@ setInterval(() => {
     const minuteDif = parseInt(timeDif/1000/60%60);
     const secondDif = parseInt(timeDif/1000%60);
 
-    document.querySelector("#timer").innerHTML = `${dayDif}:${hourDif}:${minuteDif}:${secondDif}`;
+    if (dayDif > 0) {
+        document.querySelector("#timer").innerHTML = `${dayDif}:${hourDif}:${minuteDif}:${secondDif}`;
+    }
+    else if (hourDif > 0) {
+        document.querySelector("#timer").innerHTML = `${hourDif}:${minuteDif}:${secondDif}`;
+    }
+    else if (minuteDif > 0) {
+        document.querySelector("#timer").innerHTML = `${minuteDif}:${secondDif}`;
+    }
+    else {
+        document.querySelector("#timer").innerHTML = `${secondDif}s`;
+    }
+    
+    document.querySelector("#clock").style.setProperty('background',`conic-gradient(#0f0 ${timeDif%86400000/1000/3600/24*360}deg, black 0deg)`);
+    
 }, 1000);
