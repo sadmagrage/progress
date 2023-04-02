91f1ca80b368f0805b984876200d23d11f40c152
sadmagrage <victorgoyapvgp09@gmail.com>
2023-04-02 00:07:29 -0300

    added checkpoints

--- js/js.js
+++ js/js.js
@@ -3,6 +3,32 @@ import { fetchData } from "./data.js";
 const data = await fetchData();
 const bk_time = new Date(data.ano, data.mes-1, data.dia, data.hora, data.minuto, data.segundo)
 
+const settingColor = (dayDif, timeDif) => {
+    let color;
+
+    if (dayDif == 0) {
+        color = '#fff';
+    }
+    else if (dayDif < 6) {
+        color = '#00ff7f'
+    }
+    else if (dayDif < 13) {
+        color = '#f00'
+    }
+    else if (dayDif < 30) {
+        color = '#0ff';
+    }
+    else if (dayDif < 90){
+        color = '#550fc1';
+    }
+    else {
+        color = '#ffd700';
+    }
+
+    document.querySelector("#clock").style.setProperty('background',`conic-gradient(${color} ${timeDif%86400000/1000/3600/24*360}deg, black 0deg)`);
+    document.querySelector("#timer").style.setProperty('color', color);
+};
+
 setInterval(() => {
     const now = new Date();
     const timeDif = now.getTime() - bk_time.getTime();
@@ -25,6 +51,6 @@ setInterval(() => {
         document.querySelector("#timer").innerHTML = `${secondDif}s`;
     }
     
-    document.querySelector("#clock").style.setProperty('background',`conic-gradient(#0f0 ${timeDif%86400000/1000/3600/24*360}deg, black 0deg)`);
+    settingColor(dayDif, timeDif);
     
 }, 1000);
