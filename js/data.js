ed2cfd4583771a7b4b6aaae4910d20f11a7920c5
sadmagrage <95193077+sadmagrage@users.noreply.github.com>
2023-06-01 01:58:53 -0300

    fixing the progress selection

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 export const fetchData = async () => {
     const data = await fetch("https://sadmagro-production.up.railway.app/progress")
         .then(res => res.json())
-        .then(data => data)
+        .then(data => data.filter(progress => progress.nome == "nfp"))
         .catch(err => console.log(err));
     
     const response = await data;
