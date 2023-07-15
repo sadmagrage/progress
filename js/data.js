b7891ce4b41066f52d919c44eea3a8a68ce8d4e7
sadmagrage <95193077+sadmagrage@users.noreply.github.com>
2023-07-15 02:47:38 -0300

    using glitch

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 export const fetchData = async () => {
-    const data = await fetch("https://sadmagro-production.up.railway.app/progress")
+    const data = await fetch("https://humorous-angry-detail.glitch.me/progress")
         .then(res => res.json())
-        .then(data => data.filter(progress => progress.nome == "nfp"))
+        .then(data => data[data.length])
         .catch(err => console.log(err));
     
     const response = await data;
